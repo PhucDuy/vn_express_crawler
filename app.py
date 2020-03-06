a5760e4155a02f4b6f12dbcf6e3a347d8371e571
Duy Phuc <phuc.tran@yomi.studio>
2020-03-06 23:03:23 +0700

    Add search form

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+
 from scripts.web_scraper import crawl_from_url
+from flask_wtf.csrf import CSRFProtect
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-BASE_URL = "https://tiki.vn/tivi/c5015?order=top_seller&src=c.5015.hamburger_menu_fly_out_banner"
+app.secret_key = 'the random string'
+csrf = CSRFProtect(app)
 
-@app.route('/')
+
+class UrlForm(FlaskForm):
+    url = StringField('url', validators=[DataRequired()])
+
+
+home_url = 'index.html'
+@app.route('/', methods=('GET', 'POST'))
 def index():
-    data = crawl_from_url(BASE_URL)
+    form = UrlForm()
+    if request.method == 'POST':
+        return search_products(form)
+    return render_template(home_url, form=form)
 
-    return render_template('index.html',data=data)
+
+def search_products(form):
+    if form.validate_on_submit():
+        print(form.url)
+        data = crawl_from_url(form.url.data)
+        return render_template(home_url, form=form, data=data)
+    return render_template(home_url, form=form)
+
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000, debug=True)
- 
+    app.run(host='127.0.0.1', port=8000, debug=True)
