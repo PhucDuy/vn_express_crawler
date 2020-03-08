c32cc3ca534cbedce6cb88afb614f6b831fc1794
Duy Phuc <phuc.tran@yomi.studio>
2020-03-08 17:48:50 +0700

    Link home page with products page

--- app.py
+++ app.py
@@ -15,32 +15,40 @@ app.config['SECRET_KEY'] = 'a_secret_key. You may change it later'
 
 
 class UrlForm(FlaskForm):
-    url = StringField('url', validators=[DataRequired()])
+    url = StringField('url', validators=[DataRequired()], render_kw={
+                      "placeholder": "Please input the link for details!"})
 
 
-home_url = 'index.html'
-@app.route('/', methods=('GET', 'POST'))
-def index():
+@app.route('/home', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+def home():
     form = UrlForm()
-    if request.method == 'POST':
-        return search_products(form)
-    return render_template(home_url, form=form)
+    if form.validate_on_submit():
+        url = form.url.data
+        (df, pages) = scrape_from_url(form.url.data)
+        return redirect(url_for('products', url=url))
+    return render_template('home.html', form=form)
 
 
-def search_products(form):
+product_html = 'products.html'
+@app.route('/products/<path:url>', methods=['GET','POST'])
+def products(url):
+    print("products page: "+url)
+    form = UrlForm()
+    if (url is not None):
+        form.url.data = url
+        (df, pages) = scrape_from_url(url)
+        return render_template(product_html, form=form, data=df, pages=pages)
+
+    return render_template(product_html, form=form)
+
+@app.route('/products',methods=['POST'])
+def product():
+    form = UrlForm()
     if form.validate_on_submit():
         (df, pages) = scrape_from_url(form.url.data)
-        return render_template(home_url, form=form, data=df, pages=pages)
-    return render_template(home_url, form=form)
-
-@app.route('/home', methods=('GET','POST'))
-def home():
-    return render_template('home.html')
-
-@app.route('/load_new_page/', methods=['POST'])
-def load_new_page():
-    form = UrlForm()
-    return redirect(url_for('/', form=form))
+        return render_template(product_html, form=form, data=df, pages=pages)
+    return render_template(product_html, form=form)
 
 
 if __name__ == '__main__':
