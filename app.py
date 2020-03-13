91b0d919ab8dbd1e8986ba90ca35e6842436fe05
Duy Phuc <phuc.tran@yomi.studio>
2020-03-13 10:22:30 +0700

    Remove url path in redirect link

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 import datetime
 import time
+import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a_secret_key. You may change it later'
@@ -19,37 +20,38 @@ class UrlForm(FlaskForm):
                       "placeholder": "Search tiki products or type a tiki url!"})
 
 
-@app.route('/home', methods=['GET', 'POST'])
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
     form = UrlForm()
-    if form.validate_on_submit():
-        url = form.url.data
-        (df, pages) = scrape_from_url(form.url.data)
-        return redirect(url_for('products', url=url))
     return render_template('home.html', form=form)
 
-
-product_html = 'products.html'
-@app.route('/products/<path:url>', methods=['GET','POST'])
-def products(url):
-    print("products page: "+url)
-    form = UrlForm()
-    if (url is not None):
-        form.url.data = url
-        (df, pages) = scrape_from_url(url)
-        return render_template(product_html, form=form, data=df, pages=pages)
-
-    return render_template(product_html, form=form)
-
-@app.route('/products',methods=['POST'])
+@app.route('/products', methods=('POST','GET'))
 def product():
     form = UrlForm()
     if form.validate_on_submit():
         (df, pages) = scrape_from_url(form.url.data)
+
         return render_template(product_html, form=form, data=df, pages=pages)
     return render_template(product_html, form=form)
 
+
+product_html = 'products.html'
+@app.route('/products/<path:page_url>', methods=['GET','POST'])
+def products(page_url):
+    print("products page: "+page_url)
+    form = UrlForm()
+    if (page_url is not None):
+        form.url.data = page_url
+        (df, pages) = scrape_from_url(page_url)
+        return render_template(product_html, form=form, data=df, pages=pages)
+
+    return render_template(product_html, form=form)
+
+
+@app.route('/urls')
+def url():
+    return "<h1>"+url_for('products', url="https://tiki.vn/search=shoe&page2")+"</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
